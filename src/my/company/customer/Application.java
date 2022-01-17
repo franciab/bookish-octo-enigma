9ea6c1f5d5dd98ffcdfbe279c691a7795efed21b
franciab <francisabletreasa@gmail.com>
2022-01-17 12:47:08 +0530

    Logger and Exception handling
    Added logger and handled  exceptions caught while testing

--- src/my/company/customer/Application.java
+++ src/my/company/customer/Application.java
@@ -11,56 +11,72 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 public class Application {
+
+	private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
+
 	public static void main(String[] args) {
-		String file = args[0], inputCountry = args[2].toLowerCase();
-		List<Customer> customerList = new ArrayList<>();
-		int inputMmembershipYears;
-
-		try (BufferedReader fileBufReader = new BufferedReader(new FileReader(file))) {
-			String fileLine;
-			fileBufReader.readLine(); // To avoid the first header line to be processed.
-			while ((fileLine = fileBufReader.readLine()) != null) {
-				String[] fileLineTokens = fileLine.split(",");
-				Customer customer = new Customer(fileLineTokens[0], LocalDate.parse(fileLineTokens[1].trim()),
-						LocalDate.parse(fileLineTokens[2].trim()), fileLineTokens[3].toLowerCase(), fileLineTokens[4].equals("1"), fileLineTokens[5]);
-				
-				customer.setAge(Period.between(customer.getDateOfBirth(), LocalDate.now()).getYears());
-				customer.setYearsOfMembership(Period.between(customer.getMemberSince(), LocalDate.now()).getYears());
-				customerList.add(customer);
-			}
-
-			
-			Predicate<Customer> isAdult = c -> c.getAge() >18;
-			Predicate<Customer> hasNewsLetterSubsciption = c -> c.isSubscribingToNewsletter();
-			Predicate<Customer> countryFilter = c -> c.getCountry().equals(inputCountry);
-			
-
-			inputMmembershipYears = Integer.parseInt(args[1]);
-			Predicate<Customer> memberShiopFilter = c -> c.getYearsOfMembership() >= inputMmembershipYears; 
-			
-			Consumer<Customer> printcustomer = c ->{
-				System.out.println("Customer name: "+ c.getName() +"  Membership Number: "+c.getMembershipNumber());
-			};
-			
-			Stream<Customer> customerStream = customerList.stream()
-			.filter(hasNewsLetterSubsciption.and(isAdult).and(memberShiopFilter));
-			if(!inputCountry.equals("all")) {
-				customerStream=customerStream.filter(countryFilter);
-			}
-			customerStream.forEach(printcustomer);
 		
-		}catch(NumberFormatException e) {
-			System.out.println("Enter valid number for 'Number of years for membership'");
-		} catch (DateTimeParseException e) {
-			System.out.println("Cannot Parse Date: " + e.getMessage());
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found with exception: " + e.getMessage());
-		} catch (IOException e) {
-			System.out.println("IOExcepiton: " + e.getMessage());
+		if (args.length < 3)
+			LOGGER.log(Level.SEVERE, "Insufficient Input Arguments.");
+		else {
+			String fileName = args[0];
+			String inputCountry = args[2].toLowerCase();
+			int inputMmembershipYears = Integer.parseInt(args[1]);
+			List<Customer> customerList = getCustomerList(fileName);
+			getFilteredCustomerList(customerList, inputCountry, inputMmembershipYears);
 		}
+	}
+
+	private static List<Customer> getCustomerList(String fileName) {
+		List<Customer> customerList =  new ArrayList<>();
+		try (BufferedReader fileBufReader = new BufferedReader(new FileReader(fileName))) {
+			String fileLine;
+			while ((fileLine = fileBufReader.readLine()) != null) {
+				if (!fileLine.startsWith("#")) {
+					String[] fileLineTokens = fileLine.split(",");
+					Customer customer = new Customer(fileLineTokens[0], LocalDate.parse(fileLineTokens[1].trim()),
+							LocalDate.parse(fileLineTokens[2].trim()), fileLineTokens[3].toLowerCase(),
+							fileLineTokens[4].equals("1"), fileLineTokens[5]);
+
+					customer.setAge(Period.between(customer.getDateOfBirth(), LocalDate.now()).getYears());
+					customer.setYearsOfMembership(
+							Period.between(customer.getMemberSince(), LocalDate.now()).getYears());
+					customerList.add(customer);
+				}
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			LOGGER.log(Level.SEVERE, "Some data is missing in the input file. Please check.", e);
+		} catch (NumberFormatException e) {
+			LOGGER.log(Level.SEVERE, "Exception while parsing. Enter valid number.", e);
+		} catch (DateTimeParseException e) {
+			LOGGER.log(Level.SEVERE, "Cannot Parse Date: ", e);
+		} catch (FileNotFoundException e) {
+			LOGGER.log(Level.SEVERE, "File not found with exception: ", e);
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, null, e);
+		}
+		return customerList;
+	}
+
+	private static void getFilteredCustomerList(List<Customer> customerList, String countryName, int membershipYears) {
+		Predicate<Customer> isAdult = c -> c.getAge() > 18;
+		Predicate<Customer> hasNewsLetterSubsciption = Customer::isSubscribingToNewsletter;
+		Predicate<Customer> countryFilter = c -> c.getCountry().equals(countryName);
+		Predicate<Customer> memberShipFilter = c -> c.getYearsOfMembership() >= membershipYears;
+
+		Consumer<Customer> printCustomerList = c -> System.out.println(
+				String.format("Customer name: %s    Membership Number: %s", c.getName(), c.getMembershipNumber()));
+		Stream<Customer> customerStream = customerList.stream()
+				.filter(hasNewsLetterSubsciption.and(isAdult).and(memberShipFilter));
+		if (!countryName.equals("all")) {
+			customerStream = customerStream.filter(countryFilter);
+		}
+		customerStream.forEach(printCustomerList);
 
 	}
 }
